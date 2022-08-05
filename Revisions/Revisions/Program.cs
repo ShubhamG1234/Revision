3610c6c3e78e33256daccfecacac35d452ce342e
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-08-05 18:18:14 +0530

    declaring variables

--- Revisions/Revisions/Program.cs
+++ Revisions/Revisions/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Revision
+{
+    class variables
+    {
+        static void Main(string[] args)
+        {
+            int age = 18;
+            string myName = "Bruce";
+            bool isItRight = true;              //Declaring Variables.
+            long mobileNo = 9999999999;
+            char aLetter = 'S';
+            double theDouble = 3.14;
+
+
+            Console.WriteLine(age +"  "+ myName +"  " + mobileNo);
+
+            Console.WriteLine(age+theDouble);
+        }
+    }
+}
