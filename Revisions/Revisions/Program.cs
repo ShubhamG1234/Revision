ec853ed80c6fcb2ee58bfb09e08660e2480af1db
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-08-09 10:31:27 +0530

    use of static in field

--- Revisions/Revisions/Program.cs
+++ Revisions/Revisions/Program.cs
@@ -1,31 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
+public class Account
+{
+    public int accno;
+    public String name;
+    public static string city  = "Mumbai";
+    public Account(int accno, String name)
+    {
+        this.accno = accno;
+        this.name = name;
+    }
 
-public class Dictionary
+    public void display()
+    {
+        Console.WriteLine(accno + " " + name + " " + city);
+    }
+}
+class TestStatic
 {
     public static void Main(string[] args)
     {
-
-        Dictionary<int, string> names = new Dictionary<int,string>();      
-        names.Add(1,"Sanjay");
-        names.Add(2,"Rahul");
-        names.Add(3,"Nitish");
-        names.Add(4,"Ramesh");
-        names.Add(5,"Jack");
-        names.Remove(5);
-
-       
-       
-
-        foreach (KeyValuePair<int,string> keyValues in names)
-        {
-            Console.WriteLine(keyValues.Key+" "+ keyValues.Value);
-        }
-
-        Console.WriteLine("-------------");
-        
+        Account sbi = new Account(1, "Sanjay");
+        Account pnb = new Account(2, "Rahul");
+        sbi.display();
+        pnb.display();
 
     }
 }
-
-
