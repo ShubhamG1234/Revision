07327facf4d439aaf7eae93f27b6ddb51cd2836f
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-08-09 08:43:56 +0530

    collection type hashset

--- Revisions/Revisions/Program.cs
+++ Revisions/Revisions/Program.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class List
+public class HashSet
 {
     public static void Main(string[] args)
     {
-        //var names = new List<string>(){"Sanjay","Rahul","Nitish","Ramesh","Jack"};
+        var names = new HashSet<string>(){"Sanjay","Rahul","Nitish","Ramesh","Jack"};
 
-        var names = new List<string>();     //  list of strings 
-        names.Add( "Sanjay");
-        names.Add( "Rahul");
-        names.Add( "Nitish");
-        names.Add( "Ramesh");
-        names.Add( "Jack");
+        //var names = new HashSet<string>();     //  list of strings 
+        //names.Add( "Sanjay");
+        //names.Add( "Rahul");
+        //names.Add( "Nitish");
+        //names.Add( "Ramesh");
+        //names.Add( "Jack");
 
 
         foreach (var name in names)          // iterating list element 
